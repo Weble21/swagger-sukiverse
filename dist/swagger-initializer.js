072396127499b1600ee679e12ee6a9e57ee87a3b
Weble21 <89642807+Weble21@users.noreply.github.com>
2026-05-30 16:58:34 +0900

    point swagger UI to producition api-doc

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "https://sukiverse.onrender.com/v3/api-docs",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
